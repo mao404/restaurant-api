94b55343c5c37164cb896f81c6fb9900cae8fde1
Luis Padron <luispadron25@gmail.com>
2024-04-24 21:22:36 -0500

    Inventory page added

---
+++ client/src/pages/inventory/Inventory.jsx
@@ -0,0 +1,39 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
+import Navbar from "../../components/NavBar";
+import baseApiPath from "../../services/api";
+
+const cookies = new Cookies();
+const Inventories = () => {
+  const [inventories, setInventories] = useState([]);
+
+  useEffect(() => {
+    const fetchAllInventory = async () => {
+      try {
+        const res = await axios.get(baseApiPath + "/inventory", {
+          headers: {
+            Authorization: cookies.get("Authorization"),
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+        });
+        setInventories(res.data.data);
+      } catch (err) {
+        throw err;
+      }
+    };
+    fetchAllInventory();
+  }, []);
+  return (
+    <>
+      <Navbar />
+      <h1>Inventory</h1>
+      {inventories.map((inv) => (
+        <p>{inv.title}</p>
+      ))}
+    </>
+  );
+};
+
+export default Inventories;
